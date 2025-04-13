b9bdbed99d4b58a00cdf97a9689e1c870e2599fc
oh-min <ohyemin96@gmail.com>
2025-04-13 23:17:21 +0900

    ✨ feat[#2]: 리뷰 Mapper 인터페이스 정의
    - ReviewDtoMapper 인터페이스 정의 (내부 Dto <-> 외부 Dto 변환)
    - ReviewMapper 인터페이스 정의 (내부 Dto <-> Entity 변환)

---
+++ src/main/java/com/fouribnb/review/presentation/mapper/ReviewDtoMapper.java
@@ -0,0 +1,30 @@
+package com.fouribnb.review.presentation.mapper;
+
+import com.fouribnb.review.application.dto.requestDto.CreateReviewInternalRequest;
+import com.fouribnb.review.application.dto.responseDto.ReviewInternalResponse;
+import com.fouribnb.review.presentation.dto.requestDto.CreateReviewRequest;
+import com.fouribnb.review.presentation.dto.responseDto.ReviewResponse;
+
+public class ReviewDtoMapper {
+
+  // 외부 Dto -> 내부 Dto
+  public static CreateReviewInternalRequest toCreateInternalDto(CreateReviewRequest request) {
+    return new CreateReviewInternalRequest(
+        request.userId(),
+        request.lodgeId(),
+        request.content(),
+        request.rating()
+    );
+  }
+
+  // 내부 Dto -> 외부 Dto
+  public static ReviewResponse toResponse(ReviewInternalResponse response) {
+    return new ReviewResponse(
+        response.reviewId(),
+        response.userId(),
+        response.lodgeId(),
+        response.content(),
+        response.rating()
+    );
+  }
+}
