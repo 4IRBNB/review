6c5fbc977ec9c5364b84b26359aed87c573c8e53
oh-min <ohyemin96@gmail.com>
2025-04-13 22:52:50 +0900

    ✨ feat[#2]: 리뷰 Entity 정의
    - reviewId, userId, lodgeId, content, rating 필드 정의

---
+++ src/main/java/com/fouribnb/review/domain/entity/Review.java
@@ -0,0 +1,46 @@
+package com.fouribnb.review.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "p_review")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "review_id", nullable = false)
+  private UUID reviewId;
+
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
+
+  @Column(name = "lodge_id", nullable = false)
+  private UUID lodgeId;
+
+  @Column(name = "content", nullable = false)
+  private String content;
+
+  @Column(name = "rating", nullable = false)
+  private Long rating;
+
+  @Builder
+  public Review(Long userId, UUID lodgeId, String content, Long rating) {
+    this.userId = userId;
+    this.lodgeId = lodgeId;
+    this.content = content;
+    this.rating = rating;
+  }
+
+}
