8930d6b36957e627698e93f48979c7465f738faf
oh-min <ohyemin96@gmail.com>
2025-04-14 23:00:43 +0900

    ✨ feat[#11] : BaseEntity 상속 및 JpaConfig 클래스 추가
    - Review 엔티티에 BaseEntity 상속
    - ReviewServiceImpl에 공통모듈의 JpaConfig 클래스 추가

--- src/main/java/com/fouribnb/review/application/service/ReviewServiceImpl.java
+++ src/main/java/com/fouribnb/review/application/service/ReviewServiceImpl.java
@@ -6,45 +6,48 @@ import com.fouribnb.review.application.dto.responseDto.ReviewInternalResponse;
 import com.fouribnb.review.application.mapper.ReviewMapper;
 import com.fouribnb.review.domain.entity.Review;
 import com.fouribnb.review.domain.repository.ReviewRepository;
+import com.fourirbnb.common.config.JpaConfig;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Import(JpaConfig.class)
 public class ReviewServiceImpl implements ReviewService {
 
-  private final ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
-  @Override
-  @Transactional
-  public ReviewInternalResponse createReview(CreateReviewInternalRequest request) {
+    @Override
+    @Transactional
+    public ReviewInternalResponse createReview(CreateReviewInternalRequest request) {
 
-    Review review = ReviewMapper.toEntity(request);
-    Review saved = reviewRepository.save(review);
+        Review review = ReviewMapper.toEntity(request);
+        Review saved = reviewRepository.save(review);
 
-    return ReviewMapper.toResponse(saved);
-  }
+        return ReviewMapper.toResponse(saved);
+    }
 
-  @Override
-  public List<ReviewInternalResponse> getReviewsByLodgeId(UUID lodgeId) {
-    return reviewRepository.getAllByLodgeId(lodgeId).stream()
-        .map(ReviewMapper::toResponse)
-        .collect(Collectors.toList());
-  }
+    @Override
+    public List<ReviewInternalResponse> getReviewsByLodgeId(UUID lodgeId) {
+        return reviewRepository.getAllByLodgeId(lodgeId).stream()
+            .map(ReviewMapper::toResponse)
+            .collect(Collectors.toList());
+    }
 
-  @Override
-  @Transactional
-  public ReviewInternalResponse updateReview(UUID reviewId, UpdateReviewInternalRequest request) {
+    @Override
+    @Transactional
+    public ReviewInternalResponse updateReview(UUID reviewId, UpdateReviewInternalRequest request) {
 
-    Review review = reviewRepository.findById(reviewId)
-        .orElseThrow(() -> new IllegalArgumentException("해당하는 리뷰가 없습니다."));
+        Review review = reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new IllegalArgumentException("해당하는 리뷰가 없습니다."));
 
-    review.updateReview(request.content(), request.rating());
+        review.updateReview(request.content(), request.rating());
 
-    return ReviewMapper.toResponse(review);
-  }
+        return ReviewMapper.toResponse(review);
+    }
 }
--- src/main/java/com/fouribnb/review/domain/entity/Review.java
+++ src/main/java/com/fouribnb/review/domain/entity/Review.java
@@ -1,5 +1,6 @@
 package com.fouribnb.review.domain.entity;
 
+import com.fourirbnb.common.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,36 +17,36 @@ import lombok.NoArgsConstructor;
 @Getter
 @Table(name = "p_review")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Review {
+public class Review extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  @Column(name = "review_id", nullable = false)
-  private UUID reviewId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "review_id", nullable = false)
+    private UUID reviewId;
 
-  @Column(name = "user_id", nullable = false)
-  private Long userId;
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
 
-  @Column(name = "lodge_id", nullable = false)
-  private UUID lodgeId;
+    @Column(name = "lodge_id", nullable = false)
+    private UUID lodgeId;
 
-  @Column(name = "content", nullable = false)
-  private String content;
+    @Column(name = "content", nullable = false)
+    private String content;
 
-  @Column(name = "rating", nullable = false)
-  private Long rating;
+    @Column(name = "rating", nullable = false)
+    private Long rating;
 
-  @Builder
-  public Review(Long userId, UUID lodgeId, String content, Long rating) {
-    this.userId = userId;
-    this.lodgeId = lodgeId;
-    this.content = content;
-    this.rating = rating;
-  }
+    @Builder
+    public Review(Long userId, UUID lodgeId, String content, Long rating) {
+        this.userId = userId;
+        this.lodgeId = lodgeId;
+        this.content = content;
+        this.rating = rating;
+    }
 
-  public void updateReview(String content, Long rating) {
-    this.content = content;
-    this.rating = rating;
-  }
+    public void updateReview(String content, Long rating) {
+        this.content = content;
+        this.rating = rating;
+    }
 
 }
