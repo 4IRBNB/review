bd205ad437027055a7e73f26dca50257709ec700
oh-min <ohyemin96@gmail.com>
2025-04-14 15:32:17 +0900

    🩹 fix[#9] : 불필요한 필드 제거 (requestDTO)
    - UpdateReviewRequest, UpdateReviewInternalRequest 에서 사용하지 않는 필드(userId, lodgeId) 제거

--- src/main/java/com/fouribnb/review/application/dto/requestDto/UpdateReviewInternalRequest.java
+++ src/main/java/com/fouribnb/review/application/dto/requestDto/UpdateReviewInternalRequest.java
@@ -1,10 +1,6 @@
 package com.fouribnb.review.application.dto.requestDto;
 
-import java.util.UUID;
-
 public record UpdateReviewInternalRequest(
-    Long userId,
-    UUID lodgeId,
     String content,
     Long rating
 ) {
--- src/main/java/com/fouribnb/review/presentation/dto/requestDto/UpdateReviewRequest.java
+++ src/main/java/com/fouribnb/review/presentation/dto/requestDto/UpdateReviewRequest.java
@@ -1,10 +1,6 @@
 package com.fouribnb.review.presentation.dto.requestDto;
 
-import java.util.UUID;
-
 public record UpdateReviewRequest(
-    Long userId,
-    UUID lodgeId,
     String content,
     Long rating
 ) {
