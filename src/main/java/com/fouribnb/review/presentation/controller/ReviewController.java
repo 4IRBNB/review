5f6d58bd4b605d82bf6e4d58b104be3c2da554a2
oh-min <ohyemin96@gmail.com>
2025-04-14 15:37:27 +0900

    ✨ feat[#9] : 리뷰 수정 엔드포인트 추가
    - PUT /api/reviews/{reviewId} 추가
    - Controller에 updateReview 메서드 추가

--- src/main/java/com/fouribnb/review/presentation/controller/ReviewController.java
+++ src/main/java/com/fouribnb/review/presentation/controller/ReviewController.java
@@ -1,9 +1,11 @@
 package com.fouribnb.review.presentation.controller;
 
 import com.fouribnb.review.application.dto.requestDto.CreateReviewInternalRequest;
+import com.fouribnb.review.application.dto.requestDto.UpdateReviewInternalRequest;
 import com.fouribnb.review.application.dto.responseDto.ReviewInternalResponse;
 import com.fouribnb.review.application.service.ReviewService;
 import com.fouribnb.review.presentation.dto.requestDto.CreateReviewRequest;
+import com.fouribnb.review.presentation.dto.requestDto.UpdateReviewRequest;
 import com.fouribnb.review.presentation.dto.responseDto.ReviewResponse;
 import com.fouribnb.review.presentation.mapper.ReviewDtoMapper;
 import java.util.List;
@@ -12,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +44,16 @@ public class ReviewController {
     List<ReviewInternalResponse> internalResponse = reviewService.getReviewsByLodgeId(lodgeId);
 
     return ReviewDtoMapper.toResponseList(internalResponse);
+  }
+
+  // [리뷰 수정]
+  @PutMapping("/{reviewId}")
+  public ReviewResponse updateReview(@PathVariable UUID reviewId, @RequestBody UpdateReviewRequest request){
+
+    UpdateReviewInternalRequest internalRequest = ReviewDtoMapper.toUpdateInternalDto(request);
+
+    ReviewInternalResponse internalResponse = reviewService.updateReview(reviewId, internalRequest);
+
+    return ReviewDtoMapper.toResponse(internalResponse);
   }
 }
