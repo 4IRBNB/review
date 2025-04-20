e1e44b8194a3cdfee4a1b5d4c40b5fb9f4401e7b
oh-min <ohyemin96@gmail.com>
2025-04-21 00:44:56 +0900

    ♻️ refactor[#4] : Redis Key 변경
    - 데이터 충돌 방지를 위해 Key 값에 lodgeId 를 추가

---
+++ src/main/generated/com/fouribnb/review/domain/entity/QReview.java
@@ -0,0 +1,65 @@
+package com.fouribnb.review.domain.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = -1675916127L;
+
+    public static final QReview review = new QReview("review");
+
+    public final com.fourirbnb.common.domain.QBaseEntity _super = new com.fourirbnb.common.domain.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final NumberPath<Long> createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deletedAt = _super.deletedAt;
+
+    //inherited
+    public final NumberPath<Long> deletedBy = _super.deletedBy;
+
+    public final ComparablePath<java.util.UUID> lodgeId = createComparable("lodgeId", java.util.UUID.class);
+
+    public final NumberPath<Long> rating = createNumber("rating", Long.class);
+
+    public final ComparablePath<java.util.UUID> reviewId = createComparable("reviewId", java.util.UUID.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    //inherited
+    public final NumberPath<Long> updatedBy = _super.updatedBy;
+
+    public final NumberPath<Long> userId = createNumber("userId", Long.class);
+
+    public QReview(String variable) {
+        super(Review.class, forVariable(variable));
+    }
+
+    public QReview(Path<? extends Review> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReview(PathMetadata metadata) {
+        super(Review.class, metadata);
+    }
+
+}
+
